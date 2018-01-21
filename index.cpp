45ae2cdec5a15a64ac68fd9302c948da6ce4e84c
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-01-21 10:28:50 +0530

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a[x],i;
+cout<<"enter the size of array";
+cin>>x;
+cout<<"enter  array number";
+for(i=0;i<=x;i++)
+{
+cin>>a[i];
+}
+cout<<"the number with index ";
+for(i=0;i<=x;i++)
+cout<<"%d\t index %d"<<a[i]<<i;
+return 0;
+}
